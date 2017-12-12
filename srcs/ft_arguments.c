eabc463093c441b2dfbedeb571932db3baa0ae62
Florian MADURA <fmadura@e1r12p5.42.fr>
2017-12-12 14:03:51 +0100

    parsing

--- srcs/ft_arguments.c
+++ srcs/ft_arguments.c
@@ -6,7 +6,7 @@
 /*   By: fmadura <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/02 14:03:33 by fmadura           #+#    #+#             */
-/*   Updated: 2017/12/02 17:10:06 by fmadura          ###   ########.fr       */
+/*   Updated: 2017/12/12 14:02:37 by fmadura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,27 @@ char	ft_isargument(char c)
 			c == 'i' || c == 'o' || c == 'O' || c == 'u' || c == 'U' ||
 			c == 'x' || c == 'X' || c == 'c' || c == 'C' || c == '%'
 			? c : 0);
+}
+
+char	*ft_switch(char *line, char c, va_list ap)
+{
+	char	swi;
+	char	*tmp;
+
+	if (!(swi = ft_isargument(c)))
+		return (line);
+	else if (swi == 's' || swi == 'S')
+		tmp =  va_arg(ap, const char *);
+	else if (swi == 'p')
+		tmp = (void *);
+	else if (swi == 'e' || swi == 'E')
+		tmp = ft_itoa(va_arg(ap, int));
+	else if (swi == 'o' || swi == 'O')
+		tmp = va_arg(ap, unsigned int);
+	else if (swi == 'd' || swi == 'i')
+		tmp = ft_itoa(ap, int);
+	else if (swi == 'g' || swi == 'G')
+		tmp = va_arg(ap, const char *);
+	else if (swi == 'u' || swi == 'U')
+		tmp = va_arg(ap, const char *);
 }
--- srcs/ft_format.c
+++ srcs/ft_format.c
@@ -6,28 +6,73 @@
 /*   By: fmadura <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/02 14:06:16 by fmadura           #+#    #+#             */
-/*   Updated: 2017/12/07 13:14:33 by fmadura          ###   ########.fr       */
+/*   Updated: 2017/12/12 12:56:07 by fmadura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-char	*ft_format(const char *str, va_list ap)
+static char		*join(char *s1, char *s2, int tab)
+{
+	char	*tmp;
+
+	tmp = ft_strjoin((tab ? s1: &s1[1]), s2);
+	if (s1)
+	{
+		free(s1);
+		s1 = NULL;
+	}
+	s1 = ft_strdup(tmp);
+	free(tmp);
+	return (s1);
+}
+
+static char		*join_tab(char **tab)
 {
 	int		count;
-	char	*sub;
-	char	*store;
+	char 	*store;
 
 	count = 0;
-	while (str[count])
+	store = NULL;
+	if (tab[count])
 	{
-		if (str[count] == '%')
+		store = ft_strdup(tab[0]);
+		free(tab[0]);
+		count++;
+		while (tab[count])
 		{
-			sub = ft_strsub(str, 0, count);
-			store = ft_strjoin(sub, (char *)va_arg(ap, const char *));
-			ft_putendl(store);
+			store = join(store, tab[count], 1);
+			free(tab[count]);
+			count++;
+		}
+		free(tab);
+	}
+	return (store);
+}
+
+char			*ft_format(const char *format, va_list ap)
+{
+	char	**store;
+	int		count;
+	char	*tmp;
+
+	count = 0;
+	store = ft_strsplit(format, '%');
+	while (store[count] && store[count + 1])
+	{
+		if (store[count + 1][0] == 's')
+			store[count] = join(store[count], va_arg(ap, char *), (count == 0));
+		else if (store[count + 1][0] == 'd')
+		{
+			tmp = ft_itoa(va_arg(ap, int));
+			store[count] = join(store[count], tmp, 1);
+			free(tmp);
 		}
 		count++;
 	}
-	return (ft_strdup(str));
+	tmp = ft_strdup(&store[count][1]);
+	free(store[count]);
+	store[count] = ft_strdup(tmp);
+	free(tmp);
+	return (join_tab(store));
 }
