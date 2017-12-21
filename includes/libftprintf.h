4eadc08368b3100a38db068e7e6aabada369cbbe
Florian MADURA <fmadura@e1r6p6.42.fr>
2017-12-21 11:07:03 +0100

    before struct

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: fmadura <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 13:38:43 by fmadura           #+#    #+#             */
-/*   Updated: 2017/12/07 13:02:23 by fmadura          ###   ########.fr       */
+/*   Updated: 2017/12/21 11:02:50 by fmadura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,22 @@
 # include "libft.h"
 # include <stdarg.h>
 
-int		ft_printf(const char* format, ...);
-char	*ft_format(const char *format, va_list ap);
-char	ft_isargument(char c);
-char	ft_isflag(char c);
-int		ft_isfield(char *str);
-int		ft_isprecision(char *str);
+typedef struct	s_args
+{
+	char			*line;
+	char			*flags;
+	char			attribut;
+	int				precision;
+	int				length;
+	struct s_args	*next;
+}				t_args;
+t_args			*ft_new_arg(char *str);
+int				ft_printf(const char* format, ...);
+char			*ft_format(const char *format, va_list ap);
+const char		*ft_switch(char *line, char c, va_list ap);
+char			ft_isargument(char c);
+int				ft_haveflag(char *str);
+char			ft_isflag(char c);
+int				ft_isfield(char *str);
+int				ft_isprecision(char *str);
 #endif
